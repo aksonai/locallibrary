40f1538b6a914cb7e8d3cb8550abbcbc9bd90ced
aksonai <fatjongerra@gmail.com>
2020-12-10 19:36:50 +0100

    [ADD] session

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from catalog import views
 urlpatterns = [
     path('', views.index, name='index'), # 127.0.0.1:8000/catalog/
     path('books/', views.BookListView.as_view(), name='books'),  # 127.0.0.1:8000/catalog/books/
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'), # 127.0.0.1:8000/catalog/books/1
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
